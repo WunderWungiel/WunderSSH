88ea670bb285cf41106c35e17c3c063ca3964476
Wunder Wungiel <dredlok706@gmail.com>
2024-10-17 21:14:28 +0200

    Add Polish translation, remove default German app

--- translations/harbour-wunderssh-pl.ts
+++ translations/harbour-wunderssh-pl.ts
@@ -1,182 +1,182 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="pl_PL">
 <context>
     <name>AboutPage</name>
     <message>
         <source>About</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Donate</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Source code</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>My website</source>
-        <translation type="unfinished"></translation>
+        <translation>Informacje</translation>
     </message>
     <message>
         <source>SSH servers manager for Sailfish OS</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Made by Wunder Wungiel</source>
-        <translation type="unfinished"></translation>
+        <translation>Menedżer serwerów SSH na Sailfish OS</translation>
     </message>
     <message>
         <source>Version</source>
-        <translation type="unfinished"></translation>
+        <translation>Wersja</translation>
+    </message>
+    <message>
+        <source>Made by Wunder Wungiel</source>
+        <translation>Stworzył Wunder Wungiel</translation>
+    </message>
+    <message>
+        <source>Donate</source>
+        <translation>Wesprzyj</translation>
+    </message>
+    <message>
+        <source>Source code</source>
+        <translation>Kod źródłowy</translation>
+    </message>
+    <message>
+        <source>My website</source>
+        <translation>Moja strona internetowa</translation>
     </message>
 </context>
 <context>
     <name>AddPage</name>
     <message>
         <source>Add server</source>
-        <translation type="unfinished"></translation>
+        <translation>Dodaj serwer</translation>
     </message>
     <message>
         <source>Title</source>
-        <translation type="unfinished"></translation>
+        <translation>Nazwa</translation>
     </message>
     <message>
         <source>Host</source>
-        <translation type="unfinished"></translation>
+        <translation>Host</translation>
     </message>
     <message>
         <source>Username</source>
-        <translation type="unfinished"></translation>
+        <translation>Nazwa użytkownika</translation>
     </message>
     <message>
         <source>Params</source>
-        <translation type="unfinished"></translation>
+        <translation>Parametry</translation>
     </message>
     <message>
         <source>Port</source>
-        <translation type="unfinished"></translation>
+        <translation>Port</translation>
     </message>
     <message>
         <source>Authentication method</source>
-        <translation type="unfinished"></translation>
+        <translation>Sposób uwierzytelniania</translation>
     </message>
     <message>
         <source>Automatic</source>
-        <translation type="unfinished"></translation>
+        <translation>Automatyczny</translation>
     </message>
     <message>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Hasło</translation>
     </message>
     <message>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>Zapisz</translation>
     </message>
 </context>
 <context>
     <name>CoverPage</name>
     <message>
         <source>Servers:</source>
-        <translation type="unfinished"></translation>
+        <translation>Serwery:</translation>
     </message>
     <message>
         <source>No servers yet...</source>
-        <translation type="unfinished"></translation>
+        <translation>Na razie brak serwerów...</translation>
     </message>
 </context>
 <context>
     <name>EditPage</name>
     <message>
         <source>Error: server doesn&apos;t exist...</source>
-        <translation type="unfinished"></translation>
+        <translation>Błąd: serwer nie istnieje...</translation>
     </message>
     <message>
         <source>Edit</source>
-        <translation type="unfinished"></translation>
+        <translation>Edytuj</translation>
     </message>
     <message>
         <source>Connect</source>
-        <translation type="unfinished"></translation>
+        <translation>Połącz</translation>
     </message>
     <message>
         <source>Title</source>
-        <translation type="unfinished"></translation>
+        <translation>Nazwa</translation>
     </message>
     <message>
         <source>Host</source>
-        <translation type="unfinished"></translation>
+        <translation>Host</translation>
     </message>
     <message>
         <source>Username</source>
-        <translation type="unfinished"></translation>
+        <translation>Nazwa użytkownika</translation>
     </message>
     <message>
         <source>Params</source>
-        <translation type="unfinished"></translation>
+        <translation>Parametry</translation>
     </message>
     <message>
         <source>Port</source>
-        <translation type="unfinished"></translation>
+        <translation>Port</translation>
     </message>
     <message>
         <source>Authentication method</source>
-        <translation type="unfinished"></translation>
+        <translation>Sposób uwierzytelniania</translation>
     </message>
     <message>
         <source>Automatic</source>
-        <translation type="unfinished"></translation>
+        <translation>Automatyczny</translation>
     </message>
     <message>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Hasło</translation>
     </message>
     <message>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>Zapisz</translation>
     </message>
 </context>
 <context>
     <name>MainPage</name>
     <message>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Informacje</translation>
     </message>
     <message>
         <source>Add server</source>
-        <translation type="unfinished"></translation>
+        <translation>Dodaj serwer</translation>
     </message>
     <message>
         <source>Connect</source>
-        <translation type="unfinished"></translation>
+        <translation>Połącz</translation>
     </message>
     <message>
         <source>Edit</source>
-        <translation type="unfinished"></translation>
+        <translation>Edytuj</translation>
     </message>
     <message>
         <source>Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>Usuń</translation>
     </message>
     <message>
         <source>No servers configured yet...</source>
-        <translation type="unfinished"></translation>
+        <translation>Jeszcze nie skonfigurowano serwerów...</translation>
     </message>
     <message>
         <source>Pull down to configure one!</source>
-        <translation type="unfinished"></translation>
+        <translation>Pociągnij w dół, aby skonfigurować jeden!</translation>
     </message>
 </context>
 <context>
     <name>SettingsPage</name>
     <message>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Ustawienia</translation>
     </message>
     <message>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>Zapisz</translation>
     </message>
 </context>
 </TS>
